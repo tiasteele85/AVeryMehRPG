f0b114b2827c876060d57484f35dd962e64471a6
tia <tiasteele85@gmail.com>
2019-08-15 15:15:19 -0400

    Added Spring

---
+++ aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/AVeryMehRpgApplication.java
@@ -0,0 +1,13 @@
+package co.grandcircus.aVeryMehRPG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AVeryMehRpgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AVeryMehRpgApplication.class, args);
+	}
+
+}
