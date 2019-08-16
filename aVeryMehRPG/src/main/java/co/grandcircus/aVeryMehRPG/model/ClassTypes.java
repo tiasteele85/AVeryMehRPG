f6ae424712b86e5202e2ad063e288435885a8ca5
tia <tiasteele85@gmail.com>
2019-08-16 10:03:50 -0400

    Added class info for Json files

--- aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/model/ClassTypes.java
+++ aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/model/ClassTypes.java
@@ -1,5 +1,29 @@
 package co.grandcircus.aVeryMehRPG.model;
 
-public class ClassTypes {
 
+
+/*
+ * "name": "Barbarian",
+"url": "http://www.dnd5eapi.co/api/classes/1"
+ * 
+ */
+public class ClassTypes {
+	private String name;
+	private String url;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	
 }
--- aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/model/Classes.java
+++ aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/model/Classes.java
@@ -1,8 +1,46 @@
 package co.grandcircus.aVeryMehRPG.model;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+/*
+ * "results": [
+	{
+		"name": "Barbarian",
+		"url": "http://www.dnd5eapi.co/api/classes/1"
+	},
+ * 
+ */
+
+
 /*
  * RESPONSE CLASS
  */
-public class Classes {
 
+
+public class Classes {
+	
+	@JsonProperty("results")
+	private List<ClassTypes> classes;
+	private List<ClassTypes> availCharacterTypes;
+	public List<ClassTypes> getClasses() {
+		return classes;
+	}
+	public void setClasses(List<ClassTypes> classes) {
+		this.classes = classes;
+	}
+	
+	public void setAvailCharacterTypes() {
+		availCharacterTypes.add(classes.get(2));
+		availCharacterTypes.add(classes.get(4));
+		availCharacterTypes.add(classes.get(6));
+		availCharacterTypes.add(classes.get(9));
+	}
+	
+	public List<ClassTypes> getAvailCharacters(){
+		return availCharacterTypes;
+	}
+	
+	
 }
