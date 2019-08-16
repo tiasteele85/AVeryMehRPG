9ee26fa2c2eea2061b3be1c5d1190241d7cebe0c
shehzad <slak786@gmail.com>
2019-08-16 10:05:41 -0400

    Added Item and Starting Equiptment Classes

--- aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/model/Item.java
+++ aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/model/Item.java
@@ -1,5 +1,22 @@
 package co.grandcircus.aVeryMehRPG.model;
 
 public class Item {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Item [name=" + name + "]";
+	}
+	
+	
 
 }
--- aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/model/StartingEquipment.java
+++ aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/model/StartingEquipment.java
@@ -1,5 +1,24 @@
 package co.grandcircus.aVeryMehRPG.model;
 
+import java.util.List;
+
 public class StartingEquipment {
+	
+	private List<Item> items;
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+	@Override
+	public String toString() {
+		return "StartingEquipment [items=" + items + "]";
+	}
+	
+	
 
 }
