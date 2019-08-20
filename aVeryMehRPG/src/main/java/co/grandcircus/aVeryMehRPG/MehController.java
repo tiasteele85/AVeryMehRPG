7ef8f1e2265bb2421e246412ab6e4e53d6341373
tia <tiasteele85@gmail.com>
2019-08-20 14:44:50 -0400

    Some Stuff

--- aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/MehController.java
+++ aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/MehController.java
@@ -3,7 +3,6 @@ package co.grandcircus.aVeryMehRPG;
 import java.util.List;
 import java.util.Random;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,93 +18,87 @@ public class MehController {
 
 	@Autowired
 	ApiService apiService;
-	
+
 	DungeonMaster dm;
-	
+
 	@RequestMapping("/")
 	public ModelAndView showHome() {
 		dm = new DungeonMaster();
 		System.out.println(apiService.showAll());
-		/*System.out.println(apiService.showAllWeapons());
-		System.out.println(apiService.showCharacter(9));
-		System.out.println(apiService.chooseWeapon(apiService.showCharacter(9)));*/
-		
-		List <ClassTypes> characters = apiService.showAll();
+		/*
+		 * System.out.println(apiService.showAllWeapons());
+		 * System.out.println(apiService.showCharacter(9));
+		 * System.out.println(apiService.chooseWeapon(apiService.showCharacter(9)));
+		 */
+
+		List<ClassTypes> characters = apiService.showAll();
 		ModelAndView mv = new ModelAndView("home");
 		mv.addObject("list", characters);
 		return mv;
 	}
-	
+
 	@RequestMapping("/story")
-	public ModelAndView showStory(
-			@RequestParam(value="Character") int player) {
+	public ModelAndView showStory(@RequestParam(value = "Character") int player) {
 		Random rand = new Random();
 		int num = (rand.nextInt(12));
 		dm.setEnemy(apiService.showCharacter(num), apiService.chooseWeapon(apiService.showCharacter(num)));
 //		int index = Integer.parseInt(player.substring(player.length()-1));
 		dm.setPlayer(apiService.showCharacter(player), apiService.chooseWeapon(apiService.showCharacter(player)));
 		System.out.println(player);
-		
-		
-		
-		
-	return new ModelAndView("story", "player", dm.player.getName());
-}
-	
+
+		return new ModelAndView("story", "player", dm.player.getName());
+	}
+
 	@RequestMapping("/fight")
 	public ModelAndView showFightScene() {
-		
-		ModelAndView mv =  new ModelAndView("fight");
-		mv.addObject("player" , dm.getPlayer());
+
+		ModelAndView mv = new ModelAndView("fight");
+		mv.addObject("player", dm.getPlayer());
 		System.out.println(dm.getPlayer());
-		mv.addObject("enemy" , dm.getEnemy());
+		mv.addObject("enemy", dm.getEnemy());
 		System.out.println(dm.getEnemy());
 		return mv;
 	}
-	
+
 	@RequestMapping("/takeDamage")
-	public ModelAndView takeDamage(
-			@RequestParam(value="punch", required=false) String punchbuttonClick,
-			@RequestParam(value="kick", required=false) String kickbuttonClick) {
+	public ModelAndView takeDamage(@RequestParam(value = "punch", required = false) String punchbuttonClick,
+			@RequestParam(value = "kick", required = false) String kickbuttonClick) {
 		System.out.println(punchbuttonClick);
 		System.out.println(kickbuttonClick);
-		
-		if(punchbuttonClick != null)
-		{	
+
+		if (punchbuttonClick != null) {
 			System.out.println("Here");
 			dm.takeAPunch();
 			dm.BaseFight();
-			if(dm.player.getHealth().getHealth() == 0) {
+			if (dm.player.getHealth().getHealth() == 0) {
 				return new ModelAndView("death");
-			}else if (dm.enemy.getHealth().getHealth() == 0) {
+			} else if (dm.enemy.getHealth().getHealth() == 0) {
 				return new ModelAndView("winner");
-			}else {
-				
+			} else {
+
 				return new ModelAndView("redirect:/fight");
 			}
-		}else if (kickbuttonClick != null) {
-			
+		} else if (kickbuttonClick != null) {
+
 			dm.takeAKick();
 			dm.BaseFight();
-			if(dm.takeAKick().equals("You're Dead")) {
+			if (dm.player.getHealth().getHealth() == 0) {
 				return new ModelAndView("death");
-			}else if (dm.BaseFight().equals("They're Dead")) {
+			} else if (dm.enemy.getHealth().getHealth() == 0) {
 				return new ModelAndView("winner");
-			}else {
-				
+			} else {
+
 				return new ModelAndView("redirect:/fight");
 			}
-		}else {
-			if(dm.takeAPunch().equals("You're Dead")) {
+		} else {
+			if (dm.player.getHealth().getHealth() == 0) {
 				return new ModelAndView("death");
-			}else if (dm.takeAPunch().equals("You're Dead")) {
+			} else if (dm.enemy.getHealth().getHealth() == 0) {
 				return new ModelAndView("winner");
-			}else {
+			} else {
 				return new ModelAndView("redirect:/fight");
 			}
 		}
-		
-		
 	}
 
 }
