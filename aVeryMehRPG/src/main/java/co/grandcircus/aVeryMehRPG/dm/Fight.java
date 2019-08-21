b42b73ba6739d59b9efb5e1f5e40583d9112e268
tia <tiasteele85@gmail.com>
2019-08-21 09:19:17 -0400

    Added dragon image

--- aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/dm/Fight.java
+++ aVeryMehRPG/src/main/java/co/grandcircus/aVeryMehRPG/dm/Fight.java
@@ -42,5 +42,34 @@ public class Fight {
 		
 		return word;
 	}
+	
+	public String kickie() {
+		Random rand = new Random();
+		int num = rand.nextInt(5) + 1;
+		String word = "";
+
+		switch (num) {
+		case 1:
+			word = "A major drop kick";
+			break;
+		case 2:
+			word = "A whipped whooper to the abdomen";
+			break;
+		case 3:
+			word = "Take 3 knuckles and a finger to the eye";
+			break;
+		case 4:
+			word = "Whoop Whoop Whoop Whoop, punch";
+			break;
+		case 5:
+			word = "Ninja fist so fast you don't know where it landed";
+			break;
+		default:
+			word = "Just a simple punch";
+			break;
+		}
+		
+		return word;
+	}
 
 }
